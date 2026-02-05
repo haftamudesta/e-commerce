bc1e12c7d51eab42b748d5d0c6a9d599ea1109c0
Haftamu Desta <haftamudesta>
2026-02-05 11:57:24 -0800

    Update user model

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from sqlalchemy import Integer, Column, String
+from sqlalchemy import Integer, Column, String,DateTime
+from datetime import datetime
 from app.database.database import Base
 from sqlalchemy.orm import relationship
 
@@ -11,5 +12,6 @@ class User(Base):
     email = Column(String(255), unique=True, index=True)
     hashed_password = Column(String(255))
     role = Column(String(50), default="user")
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     reviews = relationship("Review", back_populates="user", cascade="all, delete-orphan")
