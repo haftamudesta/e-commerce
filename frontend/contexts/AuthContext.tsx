3b8cd626ae403b12cb53bc0c9dfbd90f061fd75f
Haftamu Desta <haftamudesta>
2026-02-09 21:54:22 -0800

    Update auth provider

--- frontend/contexts/AuthContext.tsx
+++ frontend/contexts/AuthContext.tsx
@@ -8,7 +8,7 @@ import React, {
   ReactNode,
 } from "react";
 import { useRouter } from "next/navigation";
-import { toast } from "../hooks/use-toast";
+import { toast } from "@/hooks/use-toast";
 
 interface User {
   id: string;
