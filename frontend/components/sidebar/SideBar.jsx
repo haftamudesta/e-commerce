d3b98fd07d6cbfe7e5275100b46777e80881b53e
Haftamu Desta <haftamudesta>
2026-02-12 21:51:19 -0800

    Add side bar component

---
+++ frontend/components/sidebar/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from "next/link";
+
+function SideBar() {
+  return (
+    <main className='bg-slate-700 space-y-4'>
+        <Link href="#">Dashboard</Link>
+        <Link href="/products">Products</Link>
+        <Link href="/categories">Categories</Link>
+        <Link href="/users">Users</Link>
+        <Link href="/profile">Profile</Link>
+    </main>
+  )
+}
+
+export default SideBar
